edada13909a7ac48b9546ff88df60fabbfe653f4
JoaoVRP <joaovrp.mec.ufsc@gmail.com>
2024-01-30 16:40:37 -0300

    feat: database connection and table creation

---
+++ src/main/java/com/boardcamp/api/Models/RentalModel.java
@@ -0,0 +1,49 @@
+package com.boardcamp.api.Models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+public class RentalModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(insertable = false)
+    private LocalDate rentDate = LocalDate.now();
+
+    @Column(nullable = false)
+    private Long daysRented;
+
+    @Column
+    private LocalDate returnDate;
+
+    @Column(nullable = false)
+    private double originalPrice;
+
+    @Column
+    private double delayFee = 0;
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    private CustomerModel customer;
+
+    @ManyToOne
+    @JoinColumn(name = "gameId")
+    private GameModel game;
+}
